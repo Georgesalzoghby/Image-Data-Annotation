2f18f0f3fa6f977f5cd66045ab8c1abbf553542b
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2022-02-08 09:57:27 +0100

    - Added gitignore - fixed tabs in script - added tif file

--- scripts/filenames_to_table.py
+++ scripts/filenames_to_table.py
@@ -6,5 +6,6 @@ files_list = os.listdir(INPUT_DIR)
 table = df()
 
 for file_name in files_list:
-				if file_name.endswith(".tif"):
-								print(file_name.split(sep="_"))
+    if file_name.endswith(".tif"):
+        print(file_name.split(sep="_"))
+
