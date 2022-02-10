ce68ec3f332ab1ea3d52aa893b2862f75bbdca54
Georges <georges.al-zoghby@igh.cnrs.fr>
2022-02-10 11:54:58 +0100

    filenames_to_table.py edited

--- scripts/filenames_to_table.py
+++ scripts/filenames_to_table.py
@@ -1,10 +1,13 @@
 import os
+
 from pandas import DataFrame as df
+
 INPUT_DIR = input("input directory: ")
+
 
 files_list = os.listdir(INPUT_DIR)
 table = df()
 
 for file_name in files_list:
- if file_name.endswith(".tif"):
-  print(file_name.split(sep="_"))
+    if file_name.endswith(".tif"):
+        print(file_name.split(sep="_"))
