3b67c0ee724e224f81b5aed5a0a462d9480a4d3e
Georges <georges.al-zoghby@igh.cnrs.fr>
2022-02-14 12:14:54 +0100

    code script is working for merging tables

--- scripts/filenames_to_table.py
+++ scripts/filenames_to_table.py
@@ -5,7 +5,8 @@ from pandas import DataFrame as df
 import json
 
 
-INPUT_DIR = input("input directory: ")
+##INPUT_DIR = input("input directory: ")
+INPUT_DIR = "C:\\Users\\Al Zoghby\\PycharmProjects\\Image-Data-Annotation\\assays\\CTCF-AID"
 with open(os.path.join(INPUT_DIR, 'assay_config.json'), mode="r") as config_file:
     config = json.load(config_file)
 
@@ -17,16 +18,18 @@ table = df(columns=COLUMN_NAMES)
 
 for file_name in files_list:
     if file_name.endswith(".tif"):
-
-        line = df([file_name.split(sep="_")], columns=COLUMN_NAMES)
+        line = [[file_name]+file_name.split(sep="_")]
+        line = df(line, columns=COLUMN_NAMES)
         table = pd.concat([table, line], ignore_index=True)
-
-print(table)
-probes = pd.read_csv("probes.CSV")
-
-print(probes)
-table = pd.merge(table, probes, on="probe ID", how="left")
 print(table)
 
+probe_table = pd.read_csv(".\meta_data\probe_table.csv")
+print(probe_table)
 
+probe_table_ch0 = probe_table.rename(columns=lambda x: x+"_ch0")
+probe_table_ch1 = probe_table.rename(columns=lambda x: x+"_ch1")
+table = pd.merge(table, probe_table_ch0, left_on=table["probe_ch0"].apply(lambda x: x.split(sep="-")[0]), right_on="probe ID_ch0")
+table = pd.merge(table, probe_table_ch1, left_on=table["probe_ch1"].apply(lambda x: x.split(sep="-")[0]), right_on="probe ID_ch1")
+
+print(table)
 
