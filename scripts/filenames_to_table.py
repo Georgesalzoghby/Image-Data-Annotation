ae1f6ca4eaf4ab41b9bf3bf5117e8f1da7eba3e7
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2022-02-16 16:36:15 +0100

    Generalizing tables merge

--- scripts/filenames_to_table.py
+++ scripts/filenames_to_table.py
@@ -5,12 +5,13 @@ from pandas import DataFrame as df
 import json
 
 
-##INPUT_DIR = input("input directory: ")
-INPUT_DIR = "C:\\Users\\Al Zoghby\\PycharmProjects\\Image-Data-Annotation\\assays\\CTCF-AID"
+INPUT_DIR = input("input directory: ")
+#INPUT_DIR = "C:\\Users\\Al Zoghby\\PycharmProjects\\Image-Data-Annotation\\assays\\CTCF-AID"
 with open(os.path.join(INPUT_DIR, 'assay_config.json'), mode="r") as config_file:
     config = json.load(config_file)
 
 COLUMN_NAMES = config["column_names"]
+MERGES = config["merges"]
 
 
 files_list = os.listdir(INPUT_DIR)
@@ -21,15 +22,10 @@ for file_name in files_list:
         line = [[file_name]+file_name.split(sep="_")]
         line = df(line, columns=COLUMN_NAMES)
         table = pd.concat([table, line], ignore_index=True)
-print(table)
 
-probe_table = pd.read_csv(".\meta_data\probe_table.csv")
-print(probe_table)
-
-probe_table_ch0 = probe_table.rename(columns=lambda x: x+"_ch0")
-probe_table_ch1 = probe_table.rename(columns=lambda x: x+"_ch1")
-table = pd.merge(table, probe_table_ch0, left_on=table["probe_ch0"].apply(lambda x: x.split(sep="-")[0]), right_on="probe ID_ch0")
-table = pd.merge(table, probe_table_ch1, left_on=table["probe_ch1"].apply(lambda x: x.split(sep="-")[0]), right_on="probe ID_ch1")
+for col_name, table_file in MERGES.items():
+    merge_table = pd.read_csv(os.path.join(".", "meta_data", table_file))
+    table = pd.merge(table, merge_table, left_on=col_name, right_on="on")
 
 print(table)
 
