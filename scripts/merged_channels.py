83930f2034fba3bd8af738dc3a588ed9c7b30f92
Georges <georges.al-zoghby@igh.cnrs.fr>
2022-02-18 23:04:18 +0100

    merged_channels.py in a new folder

--- scripts/merged_channels.py
+++ scripts/merged_channels.py
@@ -2,12 +2,17 @@ import os
 import pandas as pd
 import numpy as np
 import tifffile
-
-INPUT_DIR = "C:\\Users\\Al Zoghby\\PycharmProjects\\Image-Data-Annotation\\assays\\CTCF-AID"
-
-image1 = tifffile.imread(os.path.join(INPUT_DIR, '20190720_RI512_CTCF-AID_control_61b_61a_SIR_2C_ALN_THR_1_C1.tif'))
-image2 = tifffile.imread(os.path.join(INPUT_DIR, '20190720_RI512_CTCF-AID_control_61b_61a_SIR_2C_ALN_THR_1_C2.tif'))
-final_image = np.stack((image1, image2))
-
-with tifffile.TiffWriter('temp.ome.tif') as tif:
-    tif.write(final_image, metadata={'axes': 'CZYX'})
+import itertools
+from pandas import DataFrame as df
+INPUT_DIR = "C:\\Users\\Al Zoghby\\PycharmProjects\\Image-Data-Annotation\\assays\\CTCF-AID\\"
+files_list = os.listdir(INPUT_DIR)
+print(files_list[-1])
+i = 0
+for file_name in files_list[:-1]:
+    print(i)
+    n0 = tifffile.imread(os.path.join(INPUT_DIR + files_list[i]))
+    n1 = tifffile.imread(os.path.join(INPUT_DIR + files_list[i+1]))
+    final_image = np.stack((n0, n1))
+    i += 2
+    with tifffile.TiffWriter(os.path.join("C:\\Users\\Al Zoghby\\PycharmProjects\\Image-Data-Annotation\\assays\\CTCF-AID_merged\\",files_list[i])) as tif:
+        tif.write(final_image, metadata={'axes': 'CZYX'})
