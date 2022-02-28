c3bd282b5ce3c6499abe88e5e7fadece23e46cf8
Georges <georges.al-zoghby@igh.cnrs.fr>
2022-02-28 12:45:02 +0100

    loop corrected for merging images

--- scripts/merged_channels.py
+++ scripts/merged_channels.py
@@ -6,12 +6,14 @@ import tifffile
 INPUT_DIR = "C:\\Users\\Al Zoghby\\PycharmProjects\\Image-Data-Annotation\\assays\\CTCF-AID\\"
 files_list = os.listdir(INPUT_DIR)
 print(files_list[-1])
-i = 0
-for file_name in files_list[:-1]:
-    print(i)
+
+for i in range(0, len(files_list)-1, 2):
     n0 = tifffile.imread(os.path.join(INPUT_DIR + files_list[i]))
     n1 = tifffile.imread(os.path.join(INPUT_DIR + files_list[i+1]))
     final_image = np.stack((n0, n1))
-    i += 2
-    with tifffile.TiffWriter(os.path.join("C:\\Users\\Al Zoghby\\PycharmProjects\\Image-Data-Annotation\\assays\\CTCF-AID_merged\\",files_list[i])) as tif:
+    print(files_list[i])
+    print(files_list[i+1])
+    print()
+
+    with tifffile.TiffWriter(os.path.join("C:\\Users\\Al Zoghby\\PycharmProjects\\Image-Data-Annotation\\assays\\CTCF-AID_merged",files_list[i][:-7] + ".ome-tif")) as tif:
         tif.write(final_image, metadata={'axes': 'CZYX'})
