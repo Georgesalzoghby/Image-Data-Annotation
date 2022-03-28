734879ccaf8ffcb33cea85fda07044ee8f45b9d4
Georges <georges.al-zoghby@igh.cnrs.fr>
2022-03-28 12:35:57 +0200

    filenames_to_table.py updated with tables

--- scripts/filenames_to_table.py
+++ scripts/filenames_to_table.py
@@ -29,13 +29,13 @@ for file_name in files_list:
 
 for col_name, table_file in MERGES.items():
     merge_table = pd.read_csv(os.path.join(".", "meta_data", table_file))
-    table = pd.merge(table, merge_table, left_on=col_name, right_on="on", how='left', suffixes=("_ch0", "_ch1"))
-table = table.drop(columns=["on_ch0", "on_ch1"])
+    table = pd.merge(table, merge_table, left_on=col_name, right_on="on", how='left', suffixes=(" Ch0", " Ch1"))
+table = table.drop(columns=["on Ch0", "on Ch1"])
 
 if 'NPC' in INPUT_DIR:
-    table = table.drop(columns=["cluster_ESC_ch0", "cluster_ESC_ch1"])
+    table = table.drop(columns=["Cluster ESC Ch0", "Cluster ESC Ch1"])
 else:
-    table = table.drop(columns=["cluster_NPC_ch0", "cluster_NPC_ch1"])
+    table = table.drop(columns=["Cluster NPC Ch0", "Cluster NPC Ch1"])
 
 csv_header = "# header "
 for dt in list(table.dtypes):
