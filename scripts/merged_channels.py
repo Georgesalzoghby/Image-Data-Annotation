7443743505162c2785fb7a1b64cdc6e1f6b1a8a5
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2022-05-23 09:47:50 +0200

    Working script to merge channels

--- scripts/merged_channels.py
+++ scripts/merged_channels.py
@@ -1,22 +1,15 @@
 import os
 import json
 import numpy as np
-import xml.etree.ElementTree as ET
+# import xml.etree.ElementTree as ET
 import tifffile
 import xtiff
+from getpass import getpass
+from omero.gateway import BlitzGateway
 
+from meta_data.crop_to_raw import crop_to_raw, crop_to_SIR
 
-channel_name_mapping = {'683.0': 'Alexa-647', '608.0': 'ATTO-555', '435.0': 'DAPI'}
-
-# image = tifffile.TiffFile('scripts/20200726_RI512_RAD21-AID-AUX_61b-647_61a-565_DAPI_001_SIR.dv.ome.tif')
-# root = ET.fromstring(image.ome_metadata)
-# for channel in root.iter('{http://www.openmicroscopy.org/Schemas/OME/2016-06}Channel'):
-#     channel.set('Name', channel_name_mapping[channel.get('Name')])
-# for channel in root.iter('{http://www.openmicroscopy.org/Schemas/OME/2016-06}Pixels'):
-#     channel.set('SizeX', 55)
-# for channel in root.iter('{http://www.openmicroscopy.org/Schemas/OME/2016-06}Pixels'):
-#     channel.set('SizeY', 55)
-
+CHANNEL_NAME_MAPPINGS = {'683.0': 'Alexa-647', '608.0': 'ATTO-555', '435.0': 'DAPI'}
 
 INPUT_DIR = "/home/julio/Documents/data-annotation/Image-Data-Annotation/assays/CTCF-AID"
 OUTPUT_DIR = INPUT_DIR + '_merged'
@@ -27,43 +20,57 @@ with open(os.path.join(INPUT_DIR, 'assay_config.json'), mode="r") as config_file
     config = json.load(config_file)
 
 nr_channels = config["nr_channels"]
+files_set = {f[:-7] for f in os.listdir(INPUT_DIR) if f.endswith('.tif')}
 
+try:
+    # image = tifffile.TiffFile('scripts/20200726_RI512_RAD21-AID-AUX_61b-647_61a-565_DAPI_001_SIR.dv.ome.tif')
+    # root = ET.fromstring(image.ome_metadata)
+    # for channel in root.iter('{http://www.openmicroscopy.org/Schemas/OME/2016-06}Channel'):
+    #     channel.set('Name', channel_name_mapping[channel.get('Name')])
+    # for channel in root.iter('{http://www.openmicroscopy.org/Schemas/OME/2016-06}Pixels'):
+    #     channel.set('SizeX', 55)
+    # for channel in root.iter('{http://www.openmicroscopy.org/Schemas/OME/2016-06}Pixels'):
+    #     channel.set('SizeY', 55)
 
-files_list = sorted([f for f in os.listdir(INPUT_DIR) if f.endswith('.tif')])
+    conn = BlitzGateway(username=input("username: "),
+                        passwd=getpass("password: "),
+                        host="omero.mri.cnrs.fr",
+                        port=4064,
+                        group="Cavalli Lab")
+    conn.connect()
 
-# TODO: This line is extracting the metadata into a file
-# ./bftools/showinf -nopix -nocore -omexml ./Image/701139/Binary/20190720_RI512_ES-CTL_61b-647_61a-565_DAPI_001_SIR.dv > metadata.xml
+    # TODO: This line is extracting the metadata into a file
+    # ./bftools/showinf -nopix -nocore -omexml ./Image/701139/Binary/20190720_RI512_ES-CTL_61b-647_61a-565_DAPI_001_SIR.dv > metadata.xml
 
-for i in range(0, len(files_list), nr_channels):
+    for file_root in files_set:
+        image = tifffile.imread(os.path.join(INPUT_DIR, f"{file_root}_C1.tif"))
 
-    n0 = tifffile.imread(os.path.join(INPUT_DIR, files_list[i]))
-    print(files_list[i])
-    if nr_channels == 1:
-        n0 = np.expand_dims(n0, 0)
-    if nr_channels == 2:
-        n1 = tifffile.imread(os.path.join(INPUT_DIR, files_list[i+1]))
-        n0 = np.stack((n0, n1))
-        print(files_list[i + 1])
+        for ch in range(1, nr_channels):
+            new_channel = tifffile.imread(os.path.join(INPUT_DIR, f"{file_root}_C{ch + 1}.tif"))
+            image = np.stack((image, new_channel))
+        if nr_channels == 1:
+            image = np.expand_dims(image, 0)
 
-    print()
-    options = dict(photometric='minisblack', metadata={'axes': 'CZYX'})
+        print(f"Processing file {file_root}")
 
-    xtiff.to_tiff(img=n0.transpose((1, 0, 2, 3)),
-                  file=os.path.join(OUTPUT_DIR, files_list[i][:-7] + ".ome.tiff"),
-                  image_date="2019:05:29 07:11:22",
-                  channel_names=["Alexa-647", "ATTO-555"],
-                  description="My description",
-                  pixel_size=0.03999,
-                  pixel_depth=0.125,
-                  )
-    # with tifffile.TiffWriter(os.path.join(OUTPUT_DIR, files_list[i][:-7] + ".ome.tif")) as tif:
-    #     tif.write(n0, **options)
-                # photometric='minisblack',
-                # metadata={
-                #     'axis': 'CZYX',
-                #     'channels': nr_channels,
-                #     'slices': 1,
-                #     'frames': n0.shape[1] * nr_channels,
-                #     'hyperstack': True,
-                #     'loop': False
-                #     })
+        raw_image_id = crop_to_raw["_".join(file_root.split("_")[:-1])]
+        sir_image_id = crop_to_SIR["_".join(file_root.split("_")[:-1])]
+
+        raw_image = conn.getObject('Image', raw_image_id)
+        sir_image = conn.getObject('Image', sir_image_id)
+
+        options = dict(photometric='minisblack', metadata={'axes': 'CZYX'})
+
+        channel_names = [CHANNEL_NAME_MAPPINGS[str(raw_image.getChannelLabels()[c])] for c in range(nr_channels)]
+
+        xtiff.to_tiff(img=image.transpose((1, 0, 2, 3)),
+                      file=os.path.join(OUTPUT_DIR, file_root + ".ome.tiff"),
+                      image_date=str(raw_image.getDate()),
+                      channel_names=channel_names,
+                      description=sir_image.getDescription(),
+                      pixel_size=round(sir_image.getPixelSizeX(), 5),
+                      pixel_depth=round(sir_image.getPixelSizeZ(), 5),
+                      )
+
+finally:
+    conn.close()
