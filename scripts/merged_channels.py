2611f3dcfde7d9c3a130816f8e392301135bdccc
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2022-05-31 16:24:04 +0200

    Internalized function to contruct ome metadata

--- scripts/merged_channels.py
+++ scripts/merged_channels.py
@@ -1,3 +1,20 @@
+import numpy as np
+
+from typing import Optional, Sequence
+from xml.etree import ElementTree
+
+OME_TYPES = {
+    np.bool_().dtype: 'bool',
+    np.int8().dtype: 'int8',
+    np.int16().dtype: 'int16',
+    np.int32().dtype: 'int32',
+    np.uint8().dtype: 'uint8',
+    np.uint16().dtype: 'uint16',
+    np.uint32().dtype: 'uint32',
+    np.float32().dtype: 'float',
+    np.float64().dtype: 'double',
+}
+
 import os
 import json
 import numpy as np
@@ -8,11 +25,90 @@ from omero.gateway import BlitzGateway
 from meta_data.crop_to_raw import crop_to_raw, crop_to_SIR
 
 
+# The "interleaved" argument was added in version 0.7.2. To not introduce breaking changes, it is a keyword-only
+# argument and can as such be handled by ome_xml_kwargs in third-party get_ome_xml implementations that do not (yet)
+# support this argument (e.g., imctools).
+def get_ome_xml(img: np.ndarray, image_name: Optional[str], channel_names: Optional[Sequence[str]], big_endian: bool,
+                pixel_size: Optional[float], pixel_depth: Optional[float], *, interleaved: bool = False,
+                **ome_xml_kwargs) -> ElementTree.ElementTree:
+    size_t, size_z, size_c, size_y, size_x, size_s = img.shape
+    if channel_names is not None:
+        assert len(channel_names) == size_c
+    if pixel_size is not None:
+        assert pixel_size > 0.
+    if pixel_depth is not None:
+        assert pixel_depth > 0
+    ome_namespace = 'http://www.openmicroscopy.org/Schemas/OME/2016-06'
+    ome_schema_location = 'http://www.openmicroscopy.org/Schemas/OME/2016-06/ome.xsd'
+    ome_element = ElementTree.Element('OME', attrib={
+        'xmlns': ome_namespace,
+        'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance',
+        'xsi:schemaLocation': ' '.join((ome_namespace, ome_schema_location))
+    })
+    instrument_element = ElementTree.SubElement(ome_element, 'Instrument', attrib={
+        'ID': 'Instrument:0'
+    })
+    objective_element = ElementTree.SubElement(instrument_element, 'Objective', attrib={
+        'Correction': "Other",
+        'ID': 'Objective:10007',
+        'Immersion': "Oil",
+        'LensNA': "1.4",
+        'Manufacturer': "Olympus",
+        'Model': "1-U2B836",
+        'NominalMagnification': "100.0",
+        'WorkingDistance': "0.13",
+        'WorkingDistanceUnit': "mm"
+    })
+    image_element = ElementTree.SubElement(ome_element, 'Image', attrib={
+        'ID': 'Image:0',
+    })
+    acquisition_date_element = ElementTree.SubElement(image_element, "AcquisitionDate")
+    acquisition_date_element.text = str(ome_xml_kwargs["AcquisitionDate"])
+    description_element = ElementTree.SubElement(image_element, "Description")
+    description_element.text = ome_xml_kwargs['Description']
+    if image_name is not None:
+        image_element.set('Name', image_name)
+    pixels_element = ElementTree.SubElement(image_element, 'Pixels', attrib={
+        'ID': 'Pixels:0',
+        'Type': OME_TYPES[img.dtype],
+        'SizeX': str(size_x),
+        'SizeY': str(size_y),
+        'SizeC': str(size_c),
+        'SizeZ': str(size_z),
+        'SizeT': str(size_t),
+        'DimensionOrder': 'XYCZT',
+        'Interleaved': 'true' if interleaved else 'false',
+        'BigEndian': 'true' if big_endian else 'false',
+    })
+    if pixel_size is not None:
+        pixels_element.set('PhysicalSizeX', str(pixel_size))
+        pixels_element.set('PhysicalSizeXUnit', 'µm')
+        pixels_element.set('PhysicalSizeY', str(pixel_size))
+        pixels_element.set('PhysicalSizeYUnit', 'µm')
+    if pixel_depth is not None:
+        pixels_element.set('PhysicalSizeZ', str(pixel_depth))
+        pixels_element.set('PhysicalSizeZUnit', 'µm')
+    for channel_id in range(size_c):
+        channel_element = ElementTree.SubElement(pixels_element, 'Channel', attrib={
+            'ID': 'Channel:0:{:d}'.format(channel_id),
+            'SamplesPerPixel': str(size_s),
+            'EmissionWavelength': str(ome_xml_kwargs['Channel']['EmissionWavelength'][channel_id]),
+            'EmissionWavelengthUnit': ome_xml_kwargs['Channel']['EmissionWavelengthUnit'][channel_id],
+            'ExcitationWavelength': str(ome_xml_kwargs['Channel']['ExcitationWavelength'][channel_id]),
+            'ExcitationWavelengthUnit': ome_xml_kwargs['Channel']['ExcitationWavelengthUnit'][channel_id]
+            }
+        )
+        if channel_names is not None and channel_names[channel_id]:
+            channel_element.set('Name', channel_names[channel_id])
+    ElementTree.SubElement(pixels_element, 'TiffData')
+    return ElementTree.ElementTree(element=ome_element)
+
+
 # TODO: check here: https://forum.image.sc/t/python-bioformats-write-image-6d-images-series-handling/28633/4
 
 CHANNEL_NAME_MAPPINGS = {'683.0': 'Alexa-647', '608.0': 'ATTO-555', '435.0': 'DAPI'}
 
-INPUT_DIR = "/home/julio/PycharmProjects/Image-Data-Annotation/assays/CTCF-AID"
+INPUT_DIR = "/home/julio/Documents/data-annotation/Image-Data-Annotation/assays/CTCF-AID"
 OUTPUT_DIR = INPUT_DIR + '_merged'
 if not os.path.exists(OUTPUT_DIR):
     os.mkdir(OUTPUT_DIR)
@@ -121,6 +217,7 @@ try:
                       profile=xtiff.tiff.TiffProfile.OME_TIFF,
                       pixel_size=round(sir_image.getPixelSizeX(), 5),
                       pixel_depth=round(sir_image.getPixelSizeZ(), 5),
+                      ome_xml_fun=get_ome_xml,
                       **metadata
                       )
 
