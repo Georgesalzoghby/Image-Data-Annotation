fd376e24461213c9197731a83e39cac58915be9b
Julio Mateos-Langerak <julio.mateos-langerak@igh.cnrs.fr>
2022-05-31 07:38:21 +0200

    Moved to saving with tifffile but not working

--- scripts/merged_channels.py
+++ scripts/merged_channels.py
@@ -16,7 +16,7 @@ javabridge.start_vm(class_path=bioformats.JARS)
 
 CHANNEL_NAME_MAPPINGS = {'683.0': 'Alexa-647', '608.0': 'ATTO-555', '435.0': 'DAPI'}
 
-INPUT_DIR = "/home/julio/Documents/data-annotation/Image-Data-Annotation/assays/CTCF-AID"
+INPUT_DIR = "/home/julio/PycharmProjects/Image-Data-Annotation/assays/CTCF-AID"
 OUTPUT_DIR = INPUT_DIR + '_merged'
 if not os.path.exists(OUTPUT_DIR):
     os.mkdir(OUTPUT_DIR)
@@ -55,36 +55,86 @@ try:
         sir_image = conn.getObject('Image', sir_image_id)
 
         raw_image_name = raw_image.getName()
-        omexml = bioformats.OMEXML(bioformats.get_omexml_metadata(f"scripts/raw_files/{raw_image_name}"))
-
-        # Remove time from acquisition
-        omexml.Image().AcquisitionDate = omexml.Image().AcquisitionDate.split('T')[0]
+        omexml = bioformats.OMEXML(bioformats.get_omexml_metadata(f"/home/julio/PycharmProjects/Image-Data-Annotation/scripts/raw_files/{raw_image_name}"))
 
         # Remove deteted DAPI
         if nr_channels < omexml.image().Pixels.channel_count:
+            pass
             # We check if it is DAPI before removal
-            if
-
-
-        for channel in ome_xml_root.iter('{http://www.openmicroscopy.org/Schemas/OME/2016-06}Channel'):
-            channel.set('Name', CHANNEL_NAME_MAPPINGS[channel.get('Name')])
-        for channel in ome_xml_root.iter('{http://www.openmicroscopy.org/Schemas/OME/2016-06}Pixels'):
-            channel.set('SizeX', 55)
-        for channel in ome_xml_root.iter('{http://www.openmicroscopy.org/Schemas/OME/2016-06}Pixels'):
-            channel.set('SizeY', 55)
-
-        options = dict(photometric='minisblack', metadata={'axes': 'CZYX'})
+            # if
 
         channel_names = [CHANNEL_NAME_MAPPINGS[str(raw_image.getChannelLabels()[c])] for c in range(nr_channels)]
 
-        xtiff.to_tiff(img=image.transpose((1, 0, 2, 3)),
-                      file=os.path.join(OUTPUT_DIR, file_root + ".ome.tiff"),
-                      image_date=str(raw_image.getDate()),
-                      channel_names=channel_names,
-                      description=sir_image.getDescription(),
-                      pixel_size=round(sir_image.getPixelSizeX(), 5),
-                      pixel_depth=round(sir_image.getPixelSizeZ(), 5),
-                      )
+        channels = raw_image.getChannels()
+        excitation_wavelengths = [ch.getExcitationWave() for ch in channels[:nr_channels]]
+        emission_wavelengths = [ch.getEmissionWave() for ch in channels[:nr_channels]]
+
+        # for channel in ome_xml_root.iter('{http://www.openmicroscopy.org/Schemas/OME/2016-06}Channel'):
+        #     channel.set('Name', CHANNEL_NAME_MAPPINGS[channel.get('Name')])
+        # for channel in ome_xml_root.iter('{http://www.openmicroscopy.org/Schemas/OME/2016-06}Pixels'):
+        #     channel.set('SizeX', 55)
+        # for channel in ome_xml_root.iter('{http://www.openmicroscopy.org/Schemas/OME/2016-06}Pixels'):
+        #     channel.set('SizeY', 55)
+
+        metadata = {'axes': 'CZYX',
+                    # # OME attributes
+                    # 'UUID'
+                    # 'Creator'
+                    # OME.Image attributes
+                    # 'Name'
+                    # OME.Image elements
+                    'AcquisitionDate': omexml.image().AcquisitionDate.split('T')[0],  # Remove time
+                    'Description': "3-beam Structured Illumination Microscopy",
+                    # OME.Image.Pixels attributes:
+                    # 'SignificantBits'
+                    'PhysicalSizeX': 0.04,
+                    'PhysicalSizeXUnit': 'MICRON',
+                    'PhysicalSizeY': 0.04,
+                    'PhysicalSizeYUnit': 'MICRON',
+                    'PhysicalSizeZ': 0.125,
+                    'PhysicalSizeZUnit': 'MICRON',
+                    # 'TimeIncrement'
+                    # 'TimeIncrementUnit'
+                    'Plane': {
+                              'ExposureTime': [.3] * (56 * nr_channels),
+                              'ExposureTimeUnit': ['msec'] * (56 * nr_channels),
+                              # 'PositionX'
+                              # 'PositionXUnit'
+                              # 'PositionY'
+                              # 'PositionYUnit'
+                              # 'PositionZ'
+                              # 'PositionZUnit'
+                              },
+                    'Channel': {'Name': channel_names,
+                                # 'AcquisitionMode'
+                                # 'Color'
+                                # 'ContrastMethod'
+                                'EmissionWavelength': emission_wavelengths,
+                                'EmissionWavelengthUnit': ['nm'] * nr_channels,
+                                'ExcitationWavelength': excitation_wavelengths,
+                                'ExcitationWavelengthUnit': ['nm'] * nr_channels,
+                                # 'Fluor'
+                                # 'IlluminationType'
+                                # 'NDFilter': "",
+                                # 'PinholeSize'
+                                # 'PinholeSizeUnit'
+                                # 'PockelCellSetting'
+                                # 'SamplesPerPixel'
+                                }
+                    }
+
+        with tifffile.TiffWriter(os.path.join(OUTPUT_DIR, file_root + ".ome.tif")) as tif:
+            tif.write(image, **metadata)
+
+
+        # xtiff.to_tiff(img=image.transpose((1, 0, 2, 3)),
+        #               file=os.path.join(OUTPUT_DIR, file_root + ".ome.tiff"),
+        #               image_date=str(raw_image.getDate()),
+        #               channel_names=channel_names,
+        #               description=sir_image.getDescription(),
+        #               pixel_size=round(sir_image.getPixelSizeX(), 5),
+        #               pixel_depth=round(sir_image.getPixelSizeZ(), 5),
+        #               )
 
 finally:
     conn.close()
