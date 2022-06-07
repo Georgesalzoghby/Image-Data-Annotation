c2f762614fd90c16cec6ce363f06d93712f8fb7c
Julio Mateos Langerak <julio.mateos-langerak@igh.cnrs.fr>
2022-06-07 12:35:15 +0200

    Going back to inputs for connecting to omero and other minor things

--- scripts/filenames_to_table.py
+++ scripts/filenames_to_table.py
@@ -7,8 +7,8 @@ import json
 ## Define some constants
 #INPUT_IMAGE_DIR = input("input directory: ")
 # INPUT_IMAGE_DIR = os.path.join(".", "assays")
-# INPUT_IMAGE_DIR = "/home/julio/Documents/data-annotation/Image-Data-Annotation/assays"
-INPUT_IMAGE_DIR = "C:\\Users\\Al Zoghby\\PycharmProjects\\Image-Data-Annotation\\assays"
+INPUT_IMAGE_DIR = "/home/julio/Documents/data-annotation/Image-Data-Annotation/assays"
+# INPUT_IMAGE_DIR = "C:\\Users\\Al Zoghby\\PycharmProjects\\Image-Data-Annotation\\assays"
 IMAGE_FILE_EXTENSION = ".ome.tiff"
 
 
