5b6e836a4065715180fd5a1382ac06268a325ff6
julio <juiio.mateos-langerak@igh.cnrs.fr>
2022-10-25 18:21:33 +0200

    Added markers to watershed

--- scripts/Image_analysis.py
+++ scripts/Image_analysis.py
@@ -63,7 +63,7 @@ OVERLAP_PROPERTIES = (
 IMAGE_FILE_EXTENSION = "ome.tiff"
 DOMAIN_MIN_VOLUME = 200  # Minimum volume for the regions
 SUBDOMAIN_MIN_VOLUME = 36  # Minimum volume for the regions
-SUBDOMAIN_MAX_NR = 10  # Maximum number of subdomains
+SUBDOMAIN_MAX_NR = 17  # Maximum number of subdomains. Obtained from Quentins data
 SIGMA = 0.5
 PIXEL_SIZE = (.125, .04, .04)  # as ZYX
 VOXEL_VOLUME = np.prod(PIXEL_SIZE)
@@ -110,11 +110,41 @@ def process_channel(channel: np.ndarray, properties: tuple, subdomain_properties
     domain_props_df.insert(loc=0, column='roi_type', value='domain')
 
     # Detecting Subdomains
+    footprint = np.array([[[0, 0, 0, 0, 0],
+                             [0, 1, 1, 1, 0],
+                             [0, 1, 1, 1, 0],
+                             [0, 1, 1, 1, 0],
+                             [0, 0, 0, 0, 0]],
+                            [[0, 1, 1, 1, 0],
+                             [1, 1, 1, 1, 1],
+                             [1, 1, 1, 1, 1],
+                             [1, 1, 1, 1, 1],
+                             [0, 1, 1, 1, 0]],
+                            [[0, 1, 1, 1, 0],
+                             [1, 1, 1, 1, 1],
+                             [1, 1, 1, 1, 1],
+                             [1, 1, 1, 1, 1],
+                             [0, 1, 1, 1, 0]],
+                            [[0, 1, 1, 1, 0],
+                             [1, 1, 1, 1, 1],
+                             [1, 1, 1, 1, 1],
+                             [1, 1, 1, 1, 1],
+                             [0, 1, 1, 1, 0]],
+                            [[0, 0, 0, 0, 0],
+                             [0, 1, 1, 1, 0],
+                             [0, 1, 1, 1, 0],
+                             [0, 1, 1, 1, 0],
+                             [0, 0, 0, 0, 0]]])
     markers = peak_local_max(channel,
                              min_distance=3,
-                             threshhold_abs=threshold_otsu(channel),
-                             num_peaks=SUBDOMAIN_MAX_NR                             )
-    subdomain_labels = watershed(np.invert(channel), mask=domain_labels)
+                             # threshold_abs=threshold_otsu(channel),
+                             indices=False,
+                             num_peaks=SUBDOMAIN_MAX_NR,
+                             footprint=footprint,
+                             labels=domain_labels)
+    subdomain_labels = watershed(np.invert(channel),
+                                 markers=markers,
+                                 mask=domain_labels)
     if subdomain_min_volume is not None:
         subdomain_labels = remove_small_objects(subdomain_labels, connectivity=subdomain_labels.ndim,
                                                 min_size=subdomain_min_volume)
@@ -275,169 +305,3 @@ if __name__ == '__main__':
     run()
     print("done")
 
-# imsave("C:\\Users\\Al Zoghby\\PycharmProjects\\Image-Data-Annotation\\assays\\CTCF-AID_merged_matpython\\20190720_RI512_CTCF-AID_AUX-CTL_61b_61a_SIR_2C_ALN_THR_labels_1.ome-tif", img_labels)
-# print(img_labels.shape)
-
-# for prop in img_region:
-#     print('Label: {} >> Object size: {}'.format(prop.label, prop.area))
-# img_region = np.array(regionprops(img_labeled))
-
-
-#
-# n files is the length of the image c1
-# if nC == 1
-# missubdomainvolume = 36
-#
-# ######
-# alldistmatrixoverlap = [];
-# 3D-sim = 1 ; pixel size xy:0.04 | Image size xy:55
-# conv WF = 0 ; pixel size xy:0.08 | Image size xy:28
-# zsize 0.125
-# minimum volume = 200
-#
-# ##Output summary files
-# if nC == 2
-# alldistmatrixoverlap = [];
-#
-# ### Reading, filtering and segmentation of images
-#
-# filtered image : gaussian filter (imgaussfilt3) with sigma = 0.5
-# thresholded image : threshold_otsu (graythresh/imbinarize) #Binarize 2-D grayscale image or 3-D volume by thresholding
-#
-# ### extraction of segmented objects
-#
-# labeled image : measure.label (bwconncomp) #Find and count connected components in binary image
-# region image : measure.regionprops (regionprops3) # Measure properties of 3-D volumetric image regions
-# find image object size > minimumn volume
-# mask ismember(labelmatrix)
-# mask imclearborder #Suppress light structures connected to image border
-#
-# if nC == 1
-# regionprops(mask, vol, centroid, principalaxislength, surfacearea)
-# if nC == 2
-# regionprops(mask, centroid)
-#
-# only keep images with 1 segmented object when centroid is not equal to 1 and then
-# jump to the next image file
-#
-#
-#
-#
-# ### Intermingling between C1 and C2 channels
-# if number of channels= 2
-# maskmerge by adding the 2 masks, maskoverlap if maskmerge bigger than 1 , projmaskoverlap
-#
-# Overlap fraction :Jaccard similarity coefficient for image segmentation (mask1,mask2)
-# overlapfractionsummary
-# --> python: from sklearn.metrics import jaccard_score
-#
-# ## 3D distance between centroids in um ; which type M?
-# xdistance = ( centroid1 of c2 - centroid1 of c1 ) * xypixelsize (0.04 | 0.08)
-# y
-# z
-# distance = sqrt ( xdistance^2 + ydistance^2 + zdistance^2 )
-#
-# ## Matrix overlap
-# statsprojmask2 = regionprops ( 'table', projmask C1 | C2 , 'area', 'centroid' )
-# rounding to nearest decimal or integer ( xshift = nx/2 - statsprojmask2.centroid(1) then y for centroid (2)
-# shiftmatrix = circshift: shift array circularly
-# mergeimc1 = zeros (x,y) --> mergeimc1 = imadd(mergeimc1, double(shiftmatrix))
-# double: Convert symbolic values to MATLAB double precision
-# imadd: Add two images or add constant to image
-#
-# dist2D = sqrt((mask2c1.centroid(1)-mask2c2.centroid(1))^2 - (mask2c1.centroid(2)-mask2c2.centroid(2))^2
-# alldistmatrixoverlap = [alldistmatrixoverlap; dist2D];
-#
-#
-# ##  Probe structure analysis
-#  only for number of channels= 1
-# voxelvol = xy * xy * z
-#
-# #volume in um3
-# vol = statsc1.vol(1) * voxelvol
-#
-# principapaxislength in um
-# xdistc1 = statsc1.principalAxisLength(1) * xy
-# ydistc1 = statsc1.principalAxisLength(2) * xy
-# zdistc1 = statsc1.principalAxisLength(3) * z
-# principalAxisLength = sqrt(xdistc1^2 + ydistc1^2 + zdistc1^2)
-#
-# #sphericity formula; surface area to volume
-#
-# ##waterrshed segmentation
-# image of zeros = im
-# mat2gray(im) :Convert matrix to grayscale image
-# imA not im
-# Inf: Create array of all Inf values
-# A = watershed(imA) ; A not mask = 0
-#
-# connectedcA = measure.labels(A)
-# prestatA = regionprops3(connectedcA, 'vol')
-# mask idxA if prestatA.vol > minsubdomainvolume
-# statsA = regionprops3(maskA, Vol, Centroid, ConvexHull)
-# subdomaincentroid = stasA.Centroid
-#
-# #subdomainvolume = statsA{:,1}.* voxelvol
-# table
-#
-# #nsubdomains = size(statsA,1) #array size
-# table
-#
-# ## Output images
-# images with segmentation borders
-#
-# adjprojC1 = imadjust(mat2gray(projC1),[low out .05;high out 1]) projC1 == channel filtered without max  #imadjust: Adjust image intensity values or colormap
-# if nC == 2 --> adjprojC2 then overlayC1C2 = imfuse(adjprojC1,adjprojC2) ##Composite of two images
-#
-# #bwboundaries: Trace region boundaries in binary image
-# bound1 = skimage.segmentation.find_boundaries(projmask2C1, connectivity=1, mode='thick', background=0)
-# if nC == 2 --> bound2(projmask2C2) and bound3(projmaskoverlap)
-# #num2str: Convert numbers to character array
-# of = OF = 'space'num2str(overlapfraction,precision 3) #precision isMaximum number of significant digits
-#
-#
-# figure (position , coord)
-# set(gcf, 'color', 'w') #set(H,NameArray,ValueArray)
-# ax = gca Current axes or chart
-# Axes Position-Related Properties
-#
-# Set graphics object properties
-#
-# if nc == 1 --> imshow(adjprojC1)
-# colb = y
-# else
-# imshow(overlayC1C2)
-# cold =magenta
-#
-# for p = 1:length(bound1) ; boundary C1 = bound1{p} ; plot(boundaryC1(:,2), boundaryC1(:,1), 'color', colb, 'LineWidth', 2.25)
-# if nC == 2
-# for q = 1:length(bound2) ;  boundary C2 = bound1{q}; plot(boundaryC2(:,2), boundaryC1(:,1),'color', 'green', 'LineWidth', 2.25)
-# end
-# for r = 1:length(bound3) ; boundary C1 = bound1{r} ; plot(boundaryC3(:,2), boundaryC1(:,1), 'color', 'white', 'LineWidth', 3.25)
-# end
-# text(x,y, of,'Color', 'white','FontSize', 24)
-#
-# if nC == 2
-# strrep: Find and replace substrings
-# '_C1_C2_segmented.tif'
-# else
-# '_' + channel + '_segmented.tif'
-#
-# Outputimagename = fullfile(outputsubfolder,imname) #Build full file name from parts
-# print (outputimagename, '-dtiff' , '-r300')
-#
-# ##summary files
-# if nC==1  # Write table to file
-# writetable(volumesummary,fullfile(outputsubfolder,volumesammaryname))
-# writetable(principleaxieslegnthsummary,..
-# writetable(sphericitysummary,..
-# writetable(subdomainvolumesummary,..
-# writetable(nsubdomainssummary,..
-#
-# if nC==2
-# writetable(overlapfractionsummary,..
-# writetable(distancecentroidsummary,..
-#
-# #averaged image if nC==2
-# shiftoverlap = round(mean(alldistmatrixoverlap)/2)
-#
